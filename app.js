cf2386abfca786f13b970e69064826bf47785eab
arvinpaundra <arvinpaundra06@gmail.com>
2021-10-18 22:20:11 +0700

    Adding a PORT var

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.get('/', (req, res) => {
 app.use('/home', index);
 app.use('/new', newMessage);
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000/');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
 });
